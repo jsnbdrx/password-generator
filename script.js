930c6742d0b3181ade543c3785bb1212b8ad19d4
jsbdr <js.bdrx@gmail.com>
2021-11-07 16:57:24 -0600

    added psuedo code notes

--- script.js
+++ script.js
@@ -4,6 +4,23 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+ 
+
+  
+//1.prompt user for password criteria
+//  a. length 8-128
+//  b. upper case
+//  c. lower case
+//  d. number
+//  e. special character
+//2. validate input
+//3. generate password based on criteria
+//4. display password to page
+
+  return "generated password";
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
